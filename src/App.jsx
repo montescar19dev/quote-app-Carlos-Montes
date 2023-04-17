1a41827984866c4586aa5bb10dd512b03fb255aa
Carlos Augusto Montes Gil <110144806+montescar19dev@users.noreply.github.com>
2023-04-17 19:41:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,7 @@ import quotes from "./quotes.json"
 import QuoteBox from './components/QuoteBox';
 import ButtonNext from './components/ButtonNext';
 
-
-
-function App() {
-
-  console.log(quotes);
-
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const [index, setIndex] = useState(randomIndex)
-
-  const changeQuote = () => {
-      const randomIndex = Math.floor(Math.random() * quotes.length);
-      setIndex(randomIndex)
-  }
-
-  const colors = ['#593e85', '#D65DB1', '#b85a9b', '#c7603c', '#bd8317', '#99982c'];
-  const randomColor = Math.floor(Math.random() * colors.length);
-  document.body.style = `background: ${colors[randomColor]}`
-  
-  function ShareButton(props) {
+function ShareButton(props) {
   const shareText = '¡Mira esta frase inspiradora que encontré en mi app!';
   const shareUrl = 'https://quotes-app-carlos-montes.netlify.app/';
 
@@ -47,18 +29,35 @@ function App() {
     });
   };
 
-
   return (
-    <>
-      <div className='App' style={{ color: colors[randomColor] }}>
-          <QuoteBox  index={index}  />
-          <ButtonNext colors={colors} randomColor={randomColor} changeQuote={changeQuote} />
-      </div>
-<div>
+    <div>
       <button onClick={shareOnFacebook}>Compartir en Facebook</button>
       <button onClick={shareOnTwitter}>Compartir en Twitter</button>
       <button onClick={shareOnWhatsApp}>Compartir en WhatsApp</button>
     </div>
+  );
+}
+
+function App() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const [index, setIndex] = useState(randomIndex)
+
+  const changeQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setIndex(randomIndex)
+  }
+
+  const colors = ['#593e85', '#D65DB1', '#b85a9b', '#c7603c', '#bd8317', '#99982c'];
+  const randomColor = Math.floor(Math.random() * colors.length);
+  document.body.style = `background: ${colors[randomColor]}`
+
+  return (
+    <>
+      <div className='App' style={{ color: colors[randomColor] }}>
+        <QuoteBox index={index} />
+        <ButtonNext colors={colors} randomColor={randomColor} changeQuote={changeQuote} />
+        <ShareButton />
+      </div>
     </>
   );
 };
